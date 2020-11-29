6a406d0c924f45348e87c22e3d851e2b160a017a
benjamin_rowz <benjaminharringtonrose@gmail.com>
2020-11-29 10:55:49 -0500

    whole lotta boiler to get started

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+enum Routes {
+  authFlow = "authFlow",
+  Signup = "Signup",
+  Signin = "Signin",
+  mainFlow = "mainFlow",
+  Account = "Account",
+  TrackCreate = "TrackCreate",
+  trackListFlow = "trackListFlow",
+  TrackList = "TrackList",
+  TrackDetail = "TrackDetail",
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+  [Routes.authFlow]: createStackNavigator({
+    [Routes.Signup]: SignupScreen,
+    [Routes.Signin]: SigninScreen,
+  }),
+  [Routes.mainFlow]: createBottomTabNavigator({
+    [Routes.trackListFlow]: createStackNavigator({
+      [Routes.TrackList]: TrackListScreen,
+      [Routes.TrackDetail]: TrackDetailScreen,
+    }),
+    [Routes.TrackCreate]: TrackCreateScreen,
+    [Routes.Account]: AccountScreen,
+  }),
 });
+
+export default createAppContainer(switchNavigator);
